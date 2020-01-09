634351026eef2dc69be4744168b12dffdf8540f7
ogustm <osward.gust.martinez@gmail.com>
2020-01-09 10:31:05 +0100

    Updates

---
+++ precache-manifest.d8d4be132c0f7b0361d6c79e1c0244af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "250cf30d14c236685705c44f847d6808",
+    "url": "/React_Updating_State_giphyApi/index.html"
+  },
+  {
+    "revision": "867859d26b02955b47c1",
+    "url": "/React_Updating_State_giphyApi/static/css/main.ab0f0bef.chunk.css"
+  },
+  {
+    "revision": "5646742b204379e54ea8",
+    "url": "/React_Updating_State_giphyApi/static/js/2.31d8e3dc.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/React_Updating_State_giphyApi/static/js/2.31d8e3dc.chunk.js.LICENSE"
+  },
+  {
+    "revision": "867859d26b02955b47c1",
+    "url": "/React_Updating_State_giphyApi/static/js/main.a6698032.chunk.js"
+  },
+  {
+    "revision": "becea1f7b9f2473ac877",
+    "url": "/React_Updating_State_giphyApi/static/js/runtime-main.fe074708.js"
+  }
+]);
